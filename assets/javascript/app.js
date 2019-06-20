eae142688cddca9f965249e207a8ddaf313b99a8
Keiko M <vikakeiko21@gmail.com>
2019-06-20 16:16:27 -0700

    display fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,29 @@
 // Variabe for my Questions, Options, Answers
 var questions = [
-    { q: "96 % 3", o: ["12", "3", "32", "9"], a: "32" },
-    { q: "57 + 18", o: ["76", "86", "65", "75"], a: "75" },
-    { q: "What are the electric appliances that are on both side of you?", o: ["DVD player", "Air Conditioner", "Television", "Amplifier"], a: "Television" },
-    { q: "What is the currency in Japan?", o: ["DEN", "NED", "YEN", "EYN"], a: "YEN" },
-    { q: "Silver & Gold, Window & Target, Key & Oyster, Rainbow & (????)", o: ["North", "West", "East", "South"]x , a: "West" },
+    { q: "96 % 3", o: ["12", "3", "32", "9"], a: "2" },
+    { q: "57 + 18", o: ["76", "86", "65", "75"], a: "4" },
+    { q: "What are the electric appliances that are on both side of you?", o: ["DVD player", "Air Conditioner", "Television", "Amplifier"], a: "3" },
+    { q: "What is the currency in Japan?", o: ["DEN", "NED", "YEN", "EYN"], a: "3" },
+    { q: "Silver & Gold, Window & Target, Key & Oyster, Rainbow & (????)", o: ["North", "West", "East", "South"], a: "1" },
 ];
-var rightAnswers = 0;
-var wrongAnswer =0;
-var timer 
-var counter = 5;
+var rightAnswer = 0;
+var wrongAnswer = 0;
+var noAnswer = 0;
+var timer
+var counter = 20;
+var displayRightAnswer = document.getElementById("correct");
+var displayWrongAnswer = document.getElementById("wrong");
 
+// timer to start conuting while the user answer the questions 
 function startTimer() {
-    timer = setInterval(function(){
-        if(counter <= 1) {
+    timer = setInterval(function () {
+        if (counter <= 1) {
             clearInterval(timer);
-            // checkInterval();
+            alert("TIMES UP!");
         }
         counter--;
         $('#show-number').text(counter);
-    },1000)
-}
-
-var intervalId; 
-// when the uber click the button, it will start decre the time 
-$('#start').on("click", run);
-function run() {
-    startTimer();
-    
-}
-
-function decrement() {
-    number--;
-// display the decrement on html 
-    $("#show-number").html("<h2>" + number + "</h2>");
-    //  when the time is over show an alert 
-    if (number === 0){
-        alert("TIMES UP!");
-    }
+    }, 1000)
 }
 
 
@@ -45,16 +31,71 @@ function decrement() {
 var addQuestions = function () {
     // loop though the questions 
     for (let i = 0; i < questions.length; i++) {
+        var questionsIndex = i;
         // apply the questions to display
         $('#game-page').append("<h2>" + questions[i].q + "<h2>");
         // loop thought the options 
         for (let j = 0; j < questions[i].o.length; j++) {
+            var answersIndex = j;
+
             console.log(questions[i].o[j]);
             // apply the options to display 
-            $("#game-page").append(`<label><input class="question" type="radio" name="question-${i}" value="">` + questions[i].o[j] +'</label>');
+            $("#game-page").append(`<label><input class="question" type="radio" name="${questionsIndex}" value="${answersIndex}">` + questions[i].o[j] + '</label>');
         }
     }
 }
+
+// when user click the radio button, it will judge the right or wrong answer \
+var checkAnswers = function () {
+    questions.forEach(function(question,index) {
+        var answer = $(`input[name="${index}"]:checked`).attr('value');
+        console.log(question + ' q ' + question.a, 'a ' + answer);
+        if(answer === undefined) {
+            console.log('no answer');
+            noAnswer++;
+        } else if(answer === question.a) {
+            rightAnswer++;
+            console.log('good');
+        } else {
+            wrongAnswer++;
+            console.log('boo');
+        }
+    })
+    // inputs.each(function(input) {
+    //     console.log(input);
+    // })
+    // var inputs = $('input[question="1"]');
+    // inputs.each(function(input) {
+    //     console.log(input);
+    // })
+    // var questionsIndex = $(this).attr('name');
+    //     var answersIndex = $(this).attr('value');
+
+    //     console.log("question " + questionsIndex, "answer " + answersIndex);
+
+    //     if(questions[parseInt(questionsIndex)].a === answersIndex) {
+    //         console.log('good');
+    //     } else {
+    //         console.log('bad')
+    //     }
+        // if (questionsIndex === answersIndex) {
+        //     rightAnswer++;
+        //     displayRightAnswer === rightAnswer;
+        // } else {
+        //     wrongAnswer++;
+        //     displayWrongAnswer === wrongAnswer;
+        // }
+        // var userAnswer = $(".question:checked").val();
+        // //  condition to count user's answer
+        // if (userAnswer === question.a) {
+        //     rightAnswer++;
+        //     displayRightAnswer = rightAnswer;
+        // } else {
+        //     wrongAnswer++;
+        //     displayWrongAnswer = wrongAnswer;
+        // }
+}
+
 
 
 
@@ -65,11 +106,15 @@ $(document).ready(function () {
         // the start button will be disappear from te page
         $("#start-page").attr("style", "display:none");
         $("#game-page").show();
+        // startTimer();
         addQuestions();
-    
+
         // var displayContent = $("#content");
         // var timeRemaining = $("<p> Time Remaining</p>");
         // // to change the #content div
         // displayContent.append(timeRemaining);
-    })
+    });
+    $("#done").on("click", checkAnswers);
+        $("#game-page").attr("style", "display:none");
+        $("#result-page").show();
 })
