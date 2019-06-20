3ca1da9aa7e1d382390dcc64302c3eeb646df27b
Keiko M <vikakeiko21@gmail.com>
2019-06-20 13:37:41 -0700

    setinterval works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,42 @@ var questions = [
     { q: "What are the electric appliances that are on both side of you?", o: ["DVD player, Air Conditioner, Television, Amplifier,"], a: "Television" },
     { q: "What is the currency in Japan?", o: ["DEN, NED, YEN, EYN"], a: "YEN" },
     { q: "Silver & Gold, Window & Target, Key & Oyster, Rainbow & (????)", o: ["North, West, East, South"], a: "West" },
-]
+];
+var rightAnswers = 0;
+var wrongAnswer =0;
+var timer 
+var counter = 5;
+
+function startTimer() {
+    timer = setInterval(function(){
+        if(counter <= 1) {
+            clearInterval(timer);
+            // checkInterval();
+        }
+        counter--;
+        $('#show-number').text(counter);
+    },1000)
+}
+
+var intervalId; 
+// when the uber click the button, it will start decre the time 
+$('#start').on("click", run);
+function run() {
+    startTimer();
+   addQuestions();
+    // intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+    number--;
+// display the decrement on html 
+    $("#show-number").html("<h2>" + number + "</h2>");
+    //  when the time is over show an alert 
+    if (number === 0){
+        alert("TIMES UP!");
+    }
+}
+
 
 // Push questions and options to the page
 var addQuestions = function () {
@@ -23,6 +58,7 @@ var addQuestions = function () {
 }
 
 
+
 $(document).ready(function () {
 
     // When the user click the start button the function will start
@@ -31,7 +67,7 @@ $(document).ready(function () {
         $("#start-page").attr("style", "display:none");
         $("#game-page").show();
         addQuestions();
-        //    get div #contet
+    
         // var displayContent = $("#content");
         // var timeRemaining = $("<p> Time Remaining</p>");
         // // to change the #content div
