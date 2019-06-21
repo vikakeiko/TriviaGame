f73240aa348eb8b050ee2c4447c7b73a0cfbd1f4
Keiko M <vikakeiko21@gmail.com>
2019-06-20 23:19:25 -0700

    add css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,19 @@
 // Variabe for my Questions, Options, Answers
 var questions = [
     { q: "96 % 3", o: ["12", "3", "32", "9"], a: "2" },
-    { q: "57 + 18", o: ["76", "86", "65", "75"], a: "4" },
-    { q: "What are the electric appliances that are on both side of you?", o: ["DVD player", "Air Conditioner", "Television", "Amplifier"], a: "3" },
-    { q: "What is the currency in Japan?", o: ["DEN", "NED", "YEN", "EYN"], a: "3" },
+    { q: "57 + 18", o: ["76", "86", "65", "75"], a: "3" },
+    { q: "What are the electric appliances that are on both side of you?", o: ["DVD player", "Air Conditioner", "Television", "Amplifier"], a: "2" },
+    { q: "What is the currency in Japan?", o: ["DEN", "NED", "YEN", "EYN"], a: "2" },
     { q: "Silver & Gold, Window & Target, Key & Oyster, Rainbow & (????)", o: ["North", "West", "East", "South"], a: "1" },
 ];
 var rightAnswer = 0;
 var wrongAnswer = 0;
 var noAnswer = 0;
 var timer
-var counter = 20;
+var counter = 10;
 var displayRightAnswer = document.getElementById("correct");
-var displayWrongAnswer = document.getElementById("wrong");
+var displayWrongAnswer = document.getElementById("incorrect");
+var displayNoAnswer = document.getElementById("unanswered");
 
 // timer to start conuting while the user answer the questions 
 function startTimer() {
@@ -20,6 +21,9 @@ function startTimer() {
         if (counter <= 1) {
             clearInterval(timer);
             alert("TIMES UP!");
+            checkAnswers();
+            $("#game-page").attr("style", "display:none");
+            $("#result-page").show();
         }
         counter--;
         $('#show-number').text(counter);
@@ -47,57 +51,26 @@ var addQuestions = function () {
 
 // when user click the radio button, it will judge the right or wrong answer \
 var checkAnswers = function () {
-    questions.forEach(function(question,index) {
+    questions.forEach(function (question, index) {
         var answer = $(`input[name="${index}"]:checked`).attr('value');
         console.log(question + ' q ' + question.a, 'a ' + answer);
-        if(answer === undefined) {
+        if (answer === undefined) {
             console.log('no answer');
             noAnswer++;
-        } else if(answer === question.a) {
+            displayNoAnswer.textContent = noAnswer;
+        } else if (answer === question.a) {
             rightAnswer++;
+            displayRightAnswer.textContent = rightAnswer;
             console.log('good');
         } else {
             wrongAnswer++;
+            displayWrongAnswer.textContent = wrongAnswer;
             console.log('boo');
         }
     })
-    // inputs.each(function(input) {
-    //     console.log(input);
-    // })
-    // var inputs = $('input[question="1"]');
-    // inputs.each(function(input) {
-    //     console.log(input);
-    // })
-    // var questionsIndex = $(this).attr('name');
-    //     var answersIndex = $(this).attr('value');
-
-    //     console.log("question " + questionsIndex, "answer " + answersIndex);
-
-    //     if(questions[parseInt(questionsIndex)].a === answersIndex) {
-    //         console.log('good');
-    //     } else {
-    //         console.log('bad')
-    //     }
-        // if (questionsIndex === answersIndex) {
-        //     rightAnswer++;
-        //     displayRightAnswer === rightAnswer;
-        // } else {
-        //     wrongAnswer++;
-        //     displayWrongAnswer === wrongAnswer;
-        // }
-        // var userAnswer = $(".question:checked").val();
-        // //  condition to count user's answer
-        // if (userAnswer === question.a) {
-        //     rightAnswer++;
-        //     displayRightAnswer = rightAnswer;
-        // } else {
-        //     wrongAnswer++;
-        //     displayWrongAnswer = wrongAnswer;
-        // }
+    $("#game-page").attr("style", "display:none");
+    $("#result-page").show();
 }
-
-
-
 
 $(document).ready(function () {
 
@@ -106,7 +79,8 @@ $(document).ready(function () {
         // the start button will be disappear from te page
         $("#start-page").attr("style", "display:none");
         $("#game-page").show();
-        // startTimer();
+        // $("#result-page").attr("style", "display:none");
+        startTimer();
         addQuestions();
 
         // var displayContent = $("#content");
@@ -115,6 +89,4 @@ $(document).ready(function () {
         // displayContent.append(timeRemaining);
     });
     $("#done").on("click", checkAnswers);
-        $("#game-page").attr("style", "display:none");
-        $("#result-page").show();
 })
