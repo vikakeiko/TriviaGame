ab9fe01c0ef8a0ba1b7982e15b9dbc96d6bb9047
Keiko M <vikakeiko21@gmail.com>
2019-06-21 00:37:41 -0700

    fixed css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,7 @@ var rightAnswer = 0;
 var wrongAnswer = 0;
 var noAnswer = 0;
 var timer
-var counter = 10;
+var counter = 80;
 var displayRightAnswer = document.getElementById("correct");
 var displayWrongAnswer = document.getElementById("incorrect");
 var displayNoAnswer = document.getElementById("unanswered");
@@ -20,7 +20,7 @@ function startTimer() {
     timer = setInterval(function () {
         if (counter <= 1) {
             clearInterval(timer);
-            alert("TIMES UP!");
+            // alert("TIMES UP!");
             checkAnswers();
             $("#game-page").attr("style", "display:none");
             $("#result-page").show();
@@ -37,14 +37,14 @@ var addQuestions = function () {
     for (let i = 0; i < questions.length; i++) {
         var questionsIndex = i;
         // apply the questions to display
-        $('#game-page').append("<h2>" + questions[i].q + "<h2>");
+        $('#content').append("<h2>" + questions[i].q + "<h2>");
         // loop thought the options 
         for (let j = 0; j < questions[i].o.length; j++) {
             var answersIndex = j;
 
             console.log(questions[i].o[j]);
             // apply the options to display 
-            $("#game-page").append(`<label><input class="question" type="radio" name="${questionsIndex}" value="${answersIndex}">` + questions[i].o[j] + '</label>');
+            $("#content").append(`<label><input class="question" type="radio" name="${questionsIndex}" value="${answersIndex}">` + questions[i].o[j] + '</label>');
         }
     }
 }
@@ -55,17 +55,17 @@ var checkAnswers = function () {
         var answer = $(`input[name="${index}"]:checked`).attr('value');
         console.log(question + ' q ' + question.a, 'a ' + answer);
         if (answer === undefined) {
-            console.log('no answer');
+            // console.log('no answer');
             noAnswer++;
             displayNoAnswer.textContent = noAnswer;
         } else if (answer === question.a) {
             rightAnswer++;
             displayRightAnswer.textContent = rightAnswer;
-            console.log('good');
+            // console.log('good');
         } else {
             wrongAnswer++;
             displayWrongAnswer.textContent = wrongAnswer;
-            console.log('boo');
+            // console.log('boo');
         }
     })
     $("#game-page").attr("style", "display:none");
@@ -82,11 +82,7 @@ $(document).ready(function () {
         // $("#result-page").attr("style", "display:none");
         startTimer();
         addQuestions();
-
-        // var displayContent = $("#content");
-        // var timeRemaining = $("<p> Time Remaining</p>");
-        // // to change the #content div
-        // displayContent.append(timeRemaining);
     });
+    // when the user clicked the button, check answers
     $("#done").on("click", checkAnswers);
 })
